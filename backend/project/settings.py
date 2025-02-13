60ed64e99e3898d32297e625889d1f7d37143e60
72mins <72min@proton.me>
2025-02-13 20:57:30 +0100

    feat: added uv for controlling dependencies

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -15,12 +15,15 @@ DEBUG = os.getenv("DEBUG") == "True"
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 INSTALLED_APPS = [
+    # Django
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third party
+    # Local
     "apps.base_app",
 ]
 
